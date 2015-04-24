19a3a0bbc74cdba18b87b2eb7f85b4690fcdbea8
ignacy130@gmail.com <ignacy130@gmail.com>
2015-04-24 08:45:57 +0200

    birthday text

--- fbLogin.js
+++ fbLogin.js
@@ -76,10 +76,19 @@ var APP_ID = "857082814361889";
   // successful.  See statusChangeCallback() for when this call is made.
   function testAPI() {
       console.log('Welcome!  Fetching your information.... ');
-      FB.api('/me', function (response) {
+      FB.api('/me',{
+        fields: 'birthday'
+    }, function (response) {
           console.log('Successful login for: ' + response.name);
+          var bd = "";
+          if(response.birtday === Date.now()){
+              bd = "<br /> Wszystkiego najlepszego z okazji urodzin!";
+          }
+          else{
+              bd = "<br /> Nie masz dziś urodzin, miłego dnia! (lub Twoja data narodzin nie jest publiczna)";
+          }
           document.getElementById('status').innerHTML =
-              'Cześć, ' + response.name + '!';
+              'Cześć, ' + response.name + '!' + bd;
           hideLoginButton();
       });
   }
